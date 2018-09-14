9c6460dd17447c9d642814781cd99309399641ed
shixianqing <1434501995@qq.com>
2018-09-14 23:21:44 +0800

    修改测试controller

--- src/main/java/com/swagger/springbootswagger/controller/UserController.java
+++ src/main/java/com/swagger/springbootswagger/controller/UserController.java
@@ -3,6 +3,7 @@ package com.swagger.springbootswagger.controller;
 import com.swagger.springbootswagger.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,17 +20,36 @@ import java.util.Map;
 @RequestMapping(value = "/user")
 public class UserController {
 
-    @RequestMapping(value = "getUser",method = RequestMethod.POST)
-    @ApiImplicitParam(name = "用户信息",required = true)
+    @RequestMapping(value = "getUser/{id}",method = RequestMethod.GET)
+
     @ApiOperation(value = "获取用户信息")
-    public User getUser(@RequestBody User user){
+    public User getUser(@RequestParam int id){
+        User user = new User();
+        user.setUserNo(id);
+        user.setAge(12);
+        user.setUserName("lisi");
         return user;
+    }
+
+    @RequestMapping(value = "addUser",method = RequestMethod.GET)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userNo", value = "用户编号",required = true,dataType = "Integer"),
+            @ApiImplicitParam(name = "userName", value = "用户名称", required = true,dataType = "String"),
+            @ApiImplicitParam(name = "userAge",value = "用户年龄" ,required = true,dataType = "Integer")
+
+    })
+    @ApiOperation(value = "添加用户信息")
+    public Map addUser(@RequestParam Integer userNo,@RequestParam String userName,@RequestParam Integer userAge){
+        System.out.println("userNo="+userNo+",userName="+userName+",userAge="+userAge);
+        Map map = new HashMap();
+        map.put("code",200);
+        map.put("msg","添加成功");
+        return map;
     }
 
 
     @RequestMapping(value = "/modifyUser/{id}",method = RequestMethod.GET)
     @ApiOperation("修改用户信息")
-    @ApiImplicitParam(name = "用户id")
     public Map updUser(@PathVariable(required = true) int id){
         Map map = new HashMap();
         map.put("code",200);
